0355b3b10f7b438557b5deceff957d3641411dda
Ioannis Apomachos <i.apomachos@gmail.com>
2023-09-14 12:27:36 +0300

    add AutoHotKey and SQLtoXML scripts

---
+++ Various Scripts/AutoConnectVPN.ahk
@@ -0,0 +1,4 @@
+﻿Sleep, 5000
+WinActivate, WatchGuard Mobile VPN with SSL ;
+SetControlDelay -1
+ControlClick, Connect, WatchGuard Mobile VPN with SSL  ;
---
+++ Various Scripts/Export SQL to XML File and then upload to FTP Server/SQLReports.ps1
@@ -0,0 +1,47 @@
+﻿Invoke-Sqlcmd -ServerInstance 192.168.1.1 -Database DatabaseName -Query "select * from ItemTable" -Username "sa" -Password "sa" -MaxCharLength 9970000 | Format-Table -Wrap -AutoSize| Out-File -Width 500 -Encoding utf8 -filePath "C:\Temp\Item.xml"
+
+(Get-Content C:\Temp\Item.xml -raw).Replace('
+Column1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+-------                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+', '') | Set-Content C:\Temp\Item.xml
+ 
+Invoke-Sqlcmd -ServerInstance 192.168.1.1 -Database DatabaseName -Query "select * from StockTable" -Username "sa" -Password "sa" -MaxCharLength 9970000 | Format-Table -Wrap -AutoSize| Out-File -Width 500 -Encoding utf8 -filePath "C:\Temp\Stock.xml"
+ 
+ 
+ (Get-Content C:\Temp\Stock.xml -raw).Replace('
+Column1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+-------                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+', '') | Set-Content C:\Temp\Stock.xml
+
+
+
+  # Load WinSCP .NET assembly
+  Add-Type -Path "C:\WinSCP\WinSCPnet.dll"
+
+  # Setup session options
+  $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+    Protocol = [WinSCP.Protocol]::Ftp
+    HostName = "IP Address"
+    UserName = "Username"
+    Password = "Password"
+
+  }
+
+  $session = New-Object WinSCP.Session
+
+  try
+  {
+      # Connect
+      $session.Open($sessionOptions)
+
+      # Upload Items
+      $session.PutFiles("C:\Temp\Item.xml", "/documents/xml/").Check()
+
+      # Upload Stock
+      $session.PutFiles("C:\Temp\Stock.xml", "/documents/xml/").Check()
+  }
+  finally
+  {
+      # Disconnect, clean up
+      $session.Dispose()
+  }
