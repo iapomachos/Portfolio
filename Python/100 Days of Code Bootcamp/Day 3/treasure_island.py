bb8ce1a01cb7c4f0124957ea33216e18cf5cddb1
Ioannis Apomachos <144806418+iapomachos@users.noreply.github.com>
2023-09-24 13:01:58 +0300

    Update treasure_island.py

--- Python/100 Days of Code Bootcamp/Day 3/treasure_island.py
+++ Python/100 Days of Code Bootcamp/Day 3/treasure_island.py
@@ -32,9 +32,9 @@ print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
 
 direction=input("You are at a cross road. Where do you want to go? Type left of right ")
 if direction=="left":
-    sea=input("You have come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
+    swimchoice=input("You have come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
     if sea=="wait":
-        doorcolour=input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")
+        swimchoice=input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ")
         if doorcolour=="red":
             print("Burned by fire\nGame Over.")
         elif doorcolour=="yellow":
