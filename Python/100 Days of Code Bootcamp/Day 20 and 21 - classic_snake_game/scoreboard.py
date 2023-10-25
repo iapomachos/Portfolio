e3be66480a7a25bb35a46757793170c5e2447ea9
Ioannis Apomachos <i.apomachos@gmail.com>
2023-10-25 12:48:42 +0300

    add day 20&21 of python bootcamp

---
+++ Python/100 Days of Code Bootcamp/Day 20 and 21 - classic_snake_game/scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+#Scoreboard's score constant parameters
+ALIGN = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_scoreboard()
+       
+
+    def update_scoreboard(self):
+         self.write(f"Score: {self.score}",align=ALIGN, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER",align=ALIGN, font=FONT)
+
+    def increase_score(self):
+        self.score +=1
+        self.clear()
+        self.update_scoreboard()
+        
+       
