91fb777f5f8b767732c45efda3e33cd7f8d5b3c0
Ioannis Apomachos <i.apomachos@gmail.com>
2024-01-07 17:24:15 +0200

    add list comprehension as solution on day 25 python's bootcamp

--- Python/100 Days of Code Bootcamp/Day 25 - us_states_game/main.py
+++ Python/100 Days of Code Bootcamp/Day 25 - us_states_game/main.py
@@ -20,6 +20,8 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
     
     if answer_state == "Exit":
+        #List Comprehension
+        #missing_states = [state for state in all_states if state not in guessed_states]
         missing_states = []
         for state in all_states:
             if state not in guessed_states:
